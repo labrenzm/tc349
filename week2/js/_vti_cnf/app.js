cbd71b95224d031325200b7355e9c49c3b6730bb
Matthew LaBrenz <labrenzm@msu.edu>
2013-09-13 15:59:55 -0400

    week 2

---
+++ week2/js/_vti_cnf/app.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|13 Sep 2013 19:57:32 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|Matt-PC\\Matt
+vti_modifiedby:SR|Matt-PC\\Matt
+vti_timecreated:TR|13 Sep 2013 19:04:51 -0000
+vti_backlinkinfo:VX|hello.html
+vti_nexttolasttimemodified:TW|13 Sep 2013 19:56:15 -0000
+vti_cacheddtm:TX|13 Sep 2013 19:56:15 -0000
+vti_filesize:IR|503
